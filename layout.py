adc74c6df681257999fd21f6c674f44dd0b9743a
Anitej Palakodeti <74488789+anitejpalakodeti@users.noreply.github.com>
2021-09-23 16:06:32 +0200

    Add files via upload

--- layout.py
+++ layout.py
@@ -25,9 +25,14 @@ add_checkbox1 = st.sidebar.checkbox('Main Arguments',value=True,key='Argument')
 add_checkbox2 = st.sidebar.checkbox('# Citations',value=True,key='Citations')
 add_checkbox3 = st.sidebar.checkbox('Author',value=True,key='Author')
 add_checkbox4 = st.sidebar.checkbox('URL',value=True,key='URL')
-add_checkbox4 = st.sidebar.checkbox('Keywords',value=True,key='Keywords')
+add_checkbox5 = st.sidebar.checkbox('Keywords',value=True,key='Keywords')
+add_checkbox6 = st.sidebar.checkbox('Date Published',value=True,key='Date Published')
 
-df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
-                   columns=['a', 'b', 'c'])
+df = pd.DataFrame(np.array([['ans1', 'Main Arguments1', 3,'Author1','URL1','Keywords1','01-01-2019'], ['ans2', 'Main Arguments2', 6,'Author2','URL2','Keywords2','01-01-2020'], ['ans3', 'Main Arguments3', 9,'Author3','URL3','Keywords3','01-01-2021']]),
+                   columns=['Answer', 'Main Arguments', '# Citations', 'Author', 'URL', 'Keywords', 'Date Published'])
 
-st.dataframe(df)
+
+checkbox_list = [True,add_checkbox1,add_checkbox2,add_checkbox3,add_checkbox4,add_checkbox5,add_checkbox6]
+
+st.dataframe(df.loc[:,checkbox_list])
+
