cbafe41e47ed806530ef2af5ad9595a7d15819d0
Spark <40240957+vioSpark@users.noreply.github.com>
2021-09-24 13:54:22 +0200

    Vio spark ci cd dev (#3)
    * Creating CI/CD
    
    * testing how on-push works
    
    * Added dummy test
    
    * small bug-fix
    
    * py test fixed
    
    (function name changed)
    
    * added debug pypenv
    
    * workflow syntax error fixed
    
    * branch switch
    
    * pytest giving exact file
    
    * pytest giving exact file path
    
    Co-authored-by: Spark <-->

---
+++ tests.py
@@ -0,0 +1,5 @@
+# import layout
+
+# Next, we'll need to write a test. We need prefix the test function name with 'test_', since this is what pytest expects:
+def test_dummy():
+    assert True == True
